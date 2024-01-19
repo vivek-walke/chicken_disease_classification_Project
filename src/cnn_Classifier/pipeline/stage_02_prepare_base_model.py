f4e967130a9aa883c6dd09f7d4b65f609242ff01
vivek-walke <vivekwalke87@gmail.com>
2024-01-19 16:10:43 +0530

    prepare base model completed

---
+++ src/cnn_Classifier/pipeline/stage_02_prepare_base_model.py
@@ -0,0 +1,28 @@
+from cnn_Classifier.config.configuration import ConfigurationManager
+from cnn_Classifier.components.prepare_base_model import PrepareBaseModel
+from cnn_Classifier import logger
+
+
+STAGE_NAME = "PREPARE_BASE_MODEL"
+
+class PrepareBaseModelTrainingPipeline():
+    def __init__(self):
+        pass
+
+    def main (self):
+        config = ConfigurationManager()
+        prepare_base_model_config= config.get_prepare_base_model_config()
+        prepare_base_model=PrepareBaseModel(config=prepare_base_model_config)
+        prepare_base_model.get_base_model()
+        prepare_base_model.update_base_model()
+
+if __name__ == "__main__":
+    try:
+        logger.info(f"*********************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = PrepareBaseModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+    except Exception as e:
+        logger.exception(e)
+        raise e
